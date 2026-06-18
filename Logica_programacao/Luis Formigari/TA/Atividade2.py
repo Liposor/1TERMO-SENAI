c0a4313376e1241437ad51f904984d93716b9f6e
Luis Felipe Formigari <86632519+Liposor@users.noreply.github.com>
2026-06-18 10:51:24 -0300

    atividade2

---
+++ Logica_programacao/Luis Formigari/TA/Atividade2.py
@@ -0,0 +1,10 @@
+# Perfil gamer
+
+nick = input("Digite o seu nick: ")
+nivel = int(input("Digite seu nivel: "))
+print(f"O Jogador {nick} está no nivel {nivel} e pronto para a partida!")
+
+# Calculadora 
+
+valor = int(input("Digite o valor que o aluno ganha: ")) * 4
+print(f"O Aluno ganha {valor} no final do mês")
