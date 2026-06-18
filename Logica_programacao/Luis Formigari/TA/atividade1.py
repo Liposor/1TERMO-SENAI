22afb44f60fe488d20152770fd8f081d309ddace
Luis Felipe Formigari <86632519+Liposor@users.noreply.github.com>
2026-06-18 11:20:26 -0300

    Add atividade1.py with various programming tasksa

---
+++ Logica_programacao/Luis Formigari/TA/atividade1.py
@@ -0,0 +1,66 @@
+# atividad 1
+# print("Bem-vindo ao mundo da programação em Python!")
+
+
+#atividade 2 
+# print("Luis Felipe Dalfré Formigari")
+# print(16)
+
+#atividade 3
+
+# print(135 + 246)
+# print(512 - 128)
+# a = 135 + 246
+# b = 512 - 128
+# print(a)
+# print(b)
+
+# atividade 4
+# print(15 * 8)
+# print(78 / 3)
+
+# atividade 5
+# print(5 ** 3)
+
+# atividade 6
+# nome = "Luis Felipe"
+# sobrenome = "Formigari"
+
+# print(nome + " " + sobrenome)
+
+# atividade 7 
+# produzidas = int(input("peças produzidas: "))
+# defeituosas = int(input("peças defeituosas: "))
+# boas = produzidas - defeituosas
+# taxa = boas / produzidas
+
+# print("Taxa de aproveitamento:", taxa)
+
+# Atividade 8
+# print("Eu tenho", 16, "anos e, em 10 anos, terei", 16 + 10, "anos.")
+
+# atividade 9
+# custo = 250.50
+# passagem = 412.00
+# noites = 3
+# total = (custo * noites) + passagem
+
+# print("Custo total:", total)
+
+# atividade 10
+print("==========================")
+print("Relatório de Vendas")
+print("==========================")
+
+produto = "Notebook Gamer"
+quantidade = 15
+preco = 5499.50
+
+total = quantidade * preco
+
+print("Produto:", produto)
+print("Quantidade vendida:", quantidade)
+print("Preço unitário: R$", preco)
+print("Total de vendas: R$", total)
+
+print("==========================")
