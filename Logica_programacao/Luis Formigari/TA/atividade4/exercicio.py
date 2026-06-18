634b76a4171153459108b516d49b6e43dc3ae1fc
Luis Felipe Formigari <86632519+Liposor@users.noreply.github.com>
2026-06-18 10:44:43 -0300

    exercicio

---
+++ Logica_programacao/Luis Formigari/TA/atividade4/exercicio.py
@@ -0,0 +1,9 @@
+
+
+nome = input("Digite o nome da pessoa: ")
+ano_nascimento = int(input("Digite o ano de nascimento: "))
+
+ano_atual = 2026
+idade = ano_atual - ano_nascimento
+
+print(f"{nome} tem  {idade} anos.")
