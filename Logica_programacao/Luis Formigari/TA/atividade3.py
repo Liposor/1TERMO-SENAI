645d027426de616e0e18b479653b0fec966001fe
Luis Felipe Formigari <86632519+Liposor@users.noreply.github.com>
2026-06-18 10:37:56 -0300

    atividade3

---
+++ Logica_programacao/Luis Formigari/TA/atividade3.py
@@ -0,0 +1,29 @@
+# 1. O Problema da Idade
+idade = int(input("Digite sua idade: "))
+if idade >= 18:
+    print("Você é maior de idade.")
+
+# 2. A Escrita Fiel
+nome = "Mariana"
+print(f"Seja bem-vinda, {nome}!")
+
+# 3. Falta de Espaço
+numero = 10
+if numero > 5:
+    print("O número é maior que cinco.")
+elif numero <= 5:
+    print("O número é menor ou igual a cinco.")
+
+# 4. Esquecimento Fatal
+usuario = "aluno123"
+if usuario == "aluno123":
+    print("Login realizado com sucesso.")
+
+# 5. Atribuição vs. Comparação
+clima = "ensolarado"
+if clima == "chuvoso":
+    print("Leve um guarda-chuva!")
+
+# 6. Misturando Alhos com Bugalhos
+pontos = 50
+print("Parabéns! Você fez " + str(pontos) + " pontos.")
